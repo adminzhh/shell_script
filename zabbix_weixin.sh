a5e58f4ced1947870d1f64bc05dc897df85c6e22
adminzhh <zhanghehe228@163.com>
2025-03-11 14:53:50 +0800

    zabbix报警告知企业微信

--- zabbix_weixin.sh
+++ zabbix_weixin.sh
@@ -63,3 +63,40 @@ curl -s -H "Content-Type: application/json" -X POST "https://qyapi.weixin.qq.com
 
 #将报警信息写入日志文件
 echo -e "\n报警时间:$(date +%F-%H:%M)\n报警标题:${title}\n报警内容:${message}\n\n" >> /tmp/weixin_bash.log
+
+# 消息模板如下：
+# 消息类型：问题
+# 主题：服务器{HOSTNAME1}，发生故障{TRIGGER.STATUS}: {TRIGGER.NAME}！
+# 消息：=======发生了如下的报警问题================
+# 告警主机：{HOSTNAME1} 
+# 告警主机IP：{HOST.IP}
+# 告警时间：{EVENT.DATE}-{EVENT.TIME}
+# 告警等级：{TRIGGER.SEVERITY}
+# 告警信息：{TRIGGER.NAME}
+# 告警项目：{TRIGGER.KEY1}
+# 问题详情：{ITEM.NAME} : {ITEM.VALUE}
+# 当前状态：{TRIGGER.STATUS} : {ITEM.VALUE1}
+# 事件ID：{EVENT.ID}　　
+# ===========================================
+
+# 消息类型：问题恢复
+# 主题： 服务器{HOSTNAME1}，故障已恢复{TRIGGER.STATUS}: {TRIGGER.NAME}已恢复!
+# 消息：=================恢复信息===============
+# 告警主机：{HOSTNAME1}
+# 告警主机IP：{HOST.IP}
+# 告警时间：{EVENT.DATE}-{EVENT.TIME}
+# 告警等级：{TRIGGER.SEVERITY}
+# 告警信息：{TRIGGER.NAME}
+# 告警项目：{TRIGGER.KEY1}
+# 问题详情：{ITEM.NAME} : {ITEM.VALUE}
+# 当前状态：{TRIGGER.STATUS} : {ITEM.VALUE1}
+# 事件ID：{EVENT.ID}
+# =========================================
+
+
+
+
+# 最后在zabbixui界面里添加媒介，脚本执行，再给用户添加报警媒介，再改触发器动作添加定义的企业微信报警
+# 把脚本发到zabbix-server的/usr/lib/zabbix/alertscripts目录里，因为主配置文件有定义，必须把脚本放在这
+# 最后就是特别要注意权限的问题，chown zabbix.zabbix weixin.sh 再把脚本所关联的日志也设置为zabbix用户权限
+
