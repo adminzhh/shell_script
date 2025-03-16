e0165217e84c2ac8a09039b4629a663de4c9c35a
adminzhh <zhanghehe228@163.com>
2025-03-16 22:40:20 +0800

    MySQL数据备份

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 db_user="xxx"
 db_pass="xxx"
-db_name="xxx"
+db_name="xxx" 
 db_host="xxx"
 db_port=3306
 backup_dir="/mysql_backup"
-#backup_file="${db_name}_$(date +%Y%m%d%H%M%S).sql"
-backup_file="alldata_$(date +%Y%m%d%H%M%S).sql"
+#backup_db_file="${db_name}_$(date +%Y%m%d%H%M%S).sql"
+backup_all_file="alldata_$(date +%Y%m%d%H%M%S).sql"
 
 #如果备份没有就创建备份目录
 if [ ! -d "$backup_dir" ]; then
@@ -16,8 +16,8 @@ fi
 #数据备份
 echo -e "\033[34m正在努力备份所有数据，请稍等...\033[0m"
 #echo -e "\033[34m正在努力备份${db_name}库中的数据，请稍等...\033[0m"
-#mysqldump -h"$db_host" -u"$db_user" -p"$db_pass" -P"$db_port" $db_name > "$backup_dir/$backup_file" &> /dev/null
-mysqldump -h"$db_host" -u"$db_user" -p"$db_pass" -P"$db_port" -A > "$backup_dir/$backup_file"
+#mysqldump -h"$db_host" -u"$db_user" -p"$db_pass" -P"$db_port" -B $db_name > "$backup_dir/$backup_db_file" &> /dev/null
+mysqldump -h"$db_host" -u"$db_user" -p"$db_pass" -P"$db_port" -A > "$backup_dir/$backup_all_file"
 
 #检查备份是否成功
 if [ $? -eq 0 ];then
