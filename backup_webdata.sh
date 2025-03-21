53eaf7127831353a679f2719451943c319c87551
adminzhh <zhanghehe228@163.com>
2025-03-21 17:15:42 +0800

    备份web页面数据

--- backup_webdata.sh
+++ backup_webdata.sh
@@ -2,8 +2,8 @@
 sou_path=/var/www/html
 tar_path=/opt/backup_data
 date=$(date +%Y-%m-%d)
-tar -zcf ${tar_path}/web_file_${date}.tar.gz --exclude=*.tmp ${sou_path} &> /dev/null 
-file_total=$(ls ${tar_path} | wc -l)
+tar -zcf ${tar_path}/web_file_${date}.tar.gz --exclude=*.tmp ${sou_path} &> /dev/null # 过滤*.tmp的文件
+file_total=$(ls ${tar_path} | wc -l) # 统计备份目录里的备份数据的数量
 echo "${date}的文件已打tar包放入${tar_path}，目前备份文件总数是${file_total}个"
 if [ $file_total -ge 5 ];then
         scp ${tar_path}/*.tar.gz root@192.168.88.36:/backup_webdata &> /dev/null
